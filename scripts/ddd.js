790385c8f183d966fbb59c2cd73e73804cf17882
mintustry <70564305+mintustry@users.noreply.github.com>
2021-06-18 20:58:39 +1000

    Update ddd.js

--- scripts/ddd.js
+++ scripts/ddd.js
@@ -7,7 +7,7 @@ const ddd = extend(Planet, "ddd", Planets.serpulo, 3, 1, {
     hasAtmosphere: true,
     atmosphereColor: Color.valueOf("094985"),
     atmosphereRadIn: 0.02,
-    atmosphereRadOut: 0.3,
+    atmosphereRadOut: 3,
     localizedName: "ddd"
 });
 ddd.meshLoader = () => extend(HexMesh, ddd, 6, {});
