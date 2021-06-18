9f8f77031d650b99996dcd888291a4227b407a21
mintustry <70564305+mintustry@users.noreply.github.com>
2021-06-18 20:38:41 +1000

    Create ddd.js

---
+++ ddd.js
@@ -0,0 +1,12 @@
+const ddd = extend(Planet, "ddd", Planets.sun, 3, 1, {
+    generator: new SerpuloPlanetGenerator(),
+    bloom: true,
+    radius: 1,
+    accessible: true,
+    hasAtmosphere: true,
+    atmosphereColor: Color.valueOf("094985"),
+    atmosphereRadIn: 0.02,
+    atmosphereRadOut: 0.3,
+    localizedName: "ddd"
+});
+omicron.meshLoader = () => extend(HexMesh, ddd, 6, {});
